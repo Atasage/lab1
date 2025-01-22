f19853e3d215592d44b4ffc1845fca7eab672827
Atasage <121442536+Atasage@users.noreply.github.com>
2025-01-22 04:43:56 +0300

    ready

--- base.cpp
+++ base.cpp
@@ -7,3 +7,4 @@ Base::Base(const string& n) {cout << "The constructor with parameters for the Ba
 Base::Base(const Base& other) {cout << "The copy constructor for the Base class is called\n";}
 
 Base::~Base() {cout << "Called virtual destructor for Base class\n";}
+
